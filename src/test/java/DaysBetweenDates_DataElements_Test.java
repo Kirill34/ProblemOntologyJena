a40dd72d62ddd0c3f34768c20ff67c2a1df23ba5
KKS <kirill.kuliuckin2016@yandex.ru>
2021-11-01 06:46:45 +0300

    Scalar set count of elements

--- src/test/java/DaysBetweenDates_DataElements_Test.java
+++ src/test/java/DaysBetweenDates_DataElements_Test.java
@@ -1,3 +1,4 @@
+import com.sun.org.apache.bcel.internal.generic.Select;
 import org.apache.jena.base.Sys;
 import org.apache.jena.ontology.Individual;
 import org.apache.jena.rdf.model.*;
@@ -12,10 +13,41 @@ import java.util.HashMap;
 public class DaysBetweenDates_DataElements_Test extends  DaysBetweenDates_BaseTest{
 
     protected static String DATA_ELEMENTS_RULES_FILE = "rules/data_elements.rules";
+    private InfModel infModel = null;
 
     @BeforeEach
     public void init() throws FileNotFoundException {
         createModel(DATA_ELEMENTS_RULES_FILE);
+        infModel = ModelFactory.createInfModel(reasoner, inf);
+    }
+
+    @Test
+    public void entityDatePresentation()
+    {
+
+//        Resource entityDate = inf.getResource(BASE_URL+"#Entity_Date");
+//        Property property_presentationOf = infModel.getProperty(BASE_URL+"#presentationOf");
+//        Selector selector = new SimpleSelector(entityDate, property_presentationOf, );
+//        infModel.query();
+
+
+        ResIterator iter = infModel.listResourcesWithProperty(infModel.getProperty(BASE_URL+"#presentationOf"));
+        while (iter.hasNext())
+        {
+            Resource r = iter.next();
+            System.out.print(r+"\n");
+
+            String presText = r.getProperty( infModel.getProperty(BASE_URL+"#text")).getString();
+            System.out.println(presText);
+
+            String elCount = r.getProperty( infModel.getProperty(BASE_URL+"#elementCount")).getString();
+            System.out.println("Количество элементов: "+elCount);
+
+            Statement statement = r.getProperty(infModel.getProperty(BASE_URL+"#presentationOf"));
+            System.out.print(statement.getResource()+"\n");
+
+        }
+
     }
 
     @Test
@@ -27,20 +59,6 @@ public class DaysBetweenDates_DataElements_Test extends  DaysBetweenDates_BaseTe
     @AfterEach
     public void setDown()
     {
-        InfModel infModel = ModelFactory.createInfModel(reasoner, inf);
-
-        ResIterator iter = infModel.listResourcesWithProperty(infModel.getProperty(BASE_URL+"#fullName"));
-
-        while (iter.hasNext()) {
-            Resource r = iter.nextResource();
-            Property hasError_Property = infModel.getProperty(BASE_URL+"#fullName");
-            Statement value_statement = r.getProperty(hasError_Property);
-            System.out.print(value_statement.getString()+"\n");
-            System.out.print(r+"\n");
-            //Individual ind = inf.getIndividual((r.getURI()));
-
-        }
-
 
     }
 
